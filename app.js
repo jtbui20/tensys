da165c7b6444a7bfc57527089550e1fcd6b8a152
jtbui20 <jtbui20@gmail.com>
2019-07-19 20:03:18 +1000

    Transfered to nodejs

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express"),
+    app = express(),
+    fs = require("fs"),
+    bodyParser = require("body-parser")
+const port = 3000
+
+app.use(express.static('src'))
+app.use(express.static('.'))
+app.use(express.static('tenengine'))
+app.use(express.json())
+
+app.listen(port, () => console.log(port))
+
+app.get("/", function (req, res){
+    res.sendFile("index.html");
+})
+
+app.post("/save", function(req, res) {
+    var file = req.body.info.PlayerA + "v" + req.body.info.PlayerB
+    fs.writeFile("./matches/" + file + ".txt", req.body.history, (error) => {
+        if (error) throw error;
+        res.send()
+    })
+})
