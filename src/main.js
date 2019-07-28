0e9ad4b2a571c3029875ddb584c8698ae9c33c18
jtbui20 <jtbui20@gmail.com>
2019-07-28 20:37:43 +1000

    Added toggle click + more elements

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ var width = 1000,
 var view_width = 100,
     view_height = 52,
     view_xScale = 1;
-    view_yScale = 1;
+view_yScale = 1;
 
 var graphic, trendline, circles, rearground
 
@@ -23,15 +23,20 @@ SVG.on(document, "DOMContentLoaded", () => {
     document.getElementById("saveGraph").addEventListener("click", save)
     document.getElementById("loadGraph").addEventListener("click", load)
     // Initialize SVG Component
-    graphic = SVG('graphic').size(width, height).viewbox(0,(view_height) / -2, view_width, view_height)
+    graphic = SVG('graphic').size(width, height).viewbox(0, (view_height) / -2, view_width, view_height)
     // Initialize layers here
     rearground = graphic.group()
-    trendline = graphic.polyline().stroke({ width: 0.5 }).fill('none')
+    trendline = graphic.polyline().stroke({
+        width: 0.5
+    }).fill('none')
     circles = graphic.group()
     infobox = graphic.group()
     // Compulsory Initiators
-    drawScore(0)
-    rearground.line(0, 0, 1000, 0).stroke({ width: 0.2, color: "#333" })
+    drawScore(0, 0)
+    rearground.line(0, 0, 1000, 0).stroke({
+        width: 0.2,
+        color: "#333"
+    })
 })
 
 function add(player) {
@@ -44,31 +49,55 @@ function add(player) {
             c = _game.addPoint(_game.PlayerB)
             break
     }
-    drawScore(c)
+    drawScore(c, _game.PointNo)
     console.log(_game.returnScore_compressed())
 }
 
-function drawScore(condition) {
+function drawScore(condition, point) {
     // Position is the difference between scores
-    var _location = _game.score[_game.PointNo].B - _game.score[_game.PointNo].A
+    var _location = _game.score[point].B - _game.score[point].A
     // Get reference of array
     var e = trendline.array().value
-    e.push([_game.PointNo, _location])
+    e.push([point, _location])
     trendline.plot(e)
     // Prepare circle groups
     var _cs = circles.group()
-    var _c = _cs.circle(0.5).move(_game.PointNo - 0.25, _location - 0.25).fill("#f00").data({ score: _game.returnScore() })
-
-    _c.mouseover(() => {
-        infobox.rect(8, 6).move(_c.x() + 0.5, _c.y() + 0.5).fill({color: "#999", opacity: 0.6})
-        infobox.text(_c.data('score')).font({size: 2}).move(_c.x() + 1, _c.y() + 1).fill('#fff')
+    var _c = _cs.circle(0.5).move(point - 0.25, _location - 0.25).fill("#f00").data({
+        score: _game.returnScore(),
+        on: true
     })
-
-    _c.mouseout(() => {
-        infobox.clear()
+    _c.click(() => {
+        if (_c.data("on")) {
+            infobox.clear()
+            infobox.rect(10, 8).move(_c.x() + 0.5, _c.y() + 0.5).fill({
+                color: "#999",
+                opacity: 0.6
+            })
+            infobox.text(_c.data('score')).font({
+                size: 2
+            }).move(_c.x() + 1, _c.y() + 1).fill('#fff')
+            var _b = infobox.group()
+            _b.rect(5.5, 1).move(_c.x() + 1, _c.y() + 7).fill({
+                color: "#444"
+            })
+            _b.text("Add Note").font({
+                size: 1
+            }).move(_c.x() + 1.5, _c.y() + 7).fill('#fff').click(() => {
+                alert("e")
+            })
+            _c.data({
+                on: false
+            })
+        } else {
+            infobox.clear()
+            _c.data({
+                on: true
+            })
+        }
     })
     switch (condition) {
-        case 1: case 4:
+        case 1:
+        case 4:
             drawGame("green")
             break
         case 2:
@@ -81,20 +110,24 @@ function drawScore(condition) {
     }
 }
 
+
 function Scroll(direction) {
     var _vb = graphic.viewbox()
     if (direction == "in") graphic.viewbox(_vb.x - 10, _vb.y, _vb.width, _vb.height)
-    if (direction == "out")graphic.viewbox(_vb.x + 10, _vb.y, _vb.width, _vb.height)
+    if (direction == "out") graphic.viewbox(_vb.x + 10, _vb.y, _vb.width, _vb.height)
     if (direction == "up") graphic.viewbox(_vb.x, _vb.y + 10, _vb.width, _vb.height)
     if (direction == "down") graphic.viewbox(_vb.x, _vb.y - 10, _vb.width, _vb.height)
 }
 
 function drawGame(_color) {
-    rearground.line(_game.PointNo, -50, _game.PointNo, 50).stroke({width: 0.5, color: _color})
+    rearground.line(_game.PointNo, -50, _game.PointNo, 50).stroke({
+        width: 0.5,
+        color: _color
+    })
 }
 
 function resizeGraph() {
-    
+
 }
 // Make object to manage draw
 
